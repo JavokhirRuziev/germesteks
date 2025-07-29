9ab0b073bd875c586a87b3dfee6ec1fc20d294bd
JavokhirRuziev <javokhirruziev3@gmail.com>
2025-07-29 22:51:43 +0500

    Add new HTML pages for order form, products, and reviews with consistent layout and styling

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const currentPath =
+        window.location.pathname.split('/').pop() || 'index.html';
+    const navItems = document.querySelectorAll('.navigation .navbar-items');
+
+    navItems.forEach((item) => {
+        const link = item.querySelector('a');
+        const href = link
+            .getAttribute('href')
+            .replace('/pages', '')
+            .replace('/', '');
+        console.log(href, 'href');
+        console.log(currentPath, 'currentPath');
+
+        if (href === currentPath) {
+            item.classList.add('active');
+        } else {
+            item.classList.remove('active');
+        }
+    });
+});
